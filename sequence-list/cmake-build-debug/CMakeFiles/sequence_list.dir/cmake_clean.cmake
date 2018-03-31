0f7ba781cd8082cc8ce82c366993a9e7cb37f447
houor <forhouor@gmail.com>
2018-03-31 10:49:18 +0800

    add sequence list directory

---
+++ sequence-list/cmake-build-debug/CMakeFiles/sequence_list.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sequence_list.dir/main.c.o"
+  "sequence_list.pdb"
+  "sequence_list"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/sequence_list.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
